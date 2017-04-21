53b2e3dda5f8ec55867fb86439bcf3dd5a7b93fc
Tomas Strachota <tstrachota@redhat.com>
2017-04-21 16:56:18 +0200

    Full help command

--- lib/hammer_cli_experimental.rb
+++ lib/hammer_cli_experimental.rb
@@ -5,4 +5,5 @@ module HammerCLIExperimental
   require 'hammer_cli_experimental/help' if HammerCLI::Settings.get(:experimental, :enable_help)
   require 'hammer_cli_experimental/config' if HammerCLI::Settings.get(:experimental, :enable_config_command)
   require 'hammer_cli_experimental/subcommands' if HammerCLI::Settings.get(:experimental, :enable_fuzzy_subcommands)
+  require 'hammer_cli_experimental/full_help' if HammerCLI::Settings.get(:experimental, :enable_full_help_command)
 end

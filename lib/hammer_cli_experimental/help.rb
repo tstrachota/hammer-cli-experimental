0d047fb1bad4a7f5eeb4cdebe712367e8214b3de
Tomas Strachota <tstrachota@redhat.com>
2017-04-21 16:15:59 +0200

    Help uses resources from option definition

--- lib/hammer_cli_experimental/help.rb
+++ lib/hammer_cli_experimental/help.rb
@@ -2,7 +2,7 @@ require 'colorize'
 
 module HammerCLIExperimental
 
-  class CondensedBuilder < Clamp::Help::Builder
+  class CondensedBuilder < HammerCLI::Help::Builder
 
     def default_label_indent
       29
@@ -17,15 +17,15 @@ module HammerCLIExperimental
     end
 
     def add_option_list(heading, items)
-      items = items.map do |i|
-        [i.help[0], i.help[1]]
-      end
+      collect_squeezable_names(items)
+
       items = squeeze_option_flags(items)
       items.sort! do |a, b|
         a[0] <=> b[0]
       end
 
-      puts "\n#{heading}:"
+      puts
+      heading(heading)
 
       label_width = default_label_indent
       items.each do |item|
@@ -42,78 +42,73 @@ module HammerCLIExperimental
       end
     end
 
-    SQUEEZABLE = [
-      'subnet',
-      'partition-table',
-      'operatingsystem',
-      'location',
-      'organization',
-      'owner',
-      'medium',
-      'environment',
-      'hostgroup',
-      'image',
-      'model',
-      'puppet-ca-proxy',
-      'puppet-proxy',
-      'domain',
-      'compute-profile',
-      'compute-resource',
-      'architecture',
-      'realm'
-    ]
-
     private
 
-    def squeeze_option_flags(items)
-      new_items = SQUEEZABLE.map do |flag|
-        squeeze_one(items, flag)
-      end
-      new_items += items.select do |i|
-        !is_squeezable?(i)
-      end
-      new_items.compact
+    def collect_squeezable_names(items)
+      @squeezable_names = items.map { |i| i.referenced_resource }.compact.uniq
     end
 
-    def squeeze_one(items, flag)
-      # require 'pry'; binding.pry
-      sel = items.select do |i|
-        i[0] =~ /--#{flag}[ -]/
-      end
-      return nil if sel.empty?
+    attr_reader :squeezable_names
 
-      sel = sel.map do |i|
-        i[0].split(' ')[0].gsub("--#{flag}", '')
+    def squeeze_option_flags(items)
+      help_items = items.map do |i|
+        [i.help[0], i.help[1]] if !is_squeezable?(i)
+      end.compact
+      help_items += squeezable_names.map do |resoruce_name|
+        squeezed_help(select_options_for_resource(resoruce_name, items))
       end
-      sel = sel.select{ |i| !i.empty? }
+      help_items
+    end
 
-      appendix = '[' + sel.join('|') + ']'
-      # TODO: sizes get calculated wrong when the text is coloured
-      appendix = appendix.yellow
+    def select_options_for_resource(resoruce_name, options)
+      options.select { |opt| opt.referenced_resource == resoruce_name }
+    end
+
+    def squeezed_help(options)
+      prefix = common_prefix(options.map { |opt| opt.switches[0] })
+
+      appendix_items = options.map do |opt|
+        opt.switches[0][prefix.length, opt.switches[0].length]
+      end
+      appendix_items = appendix_items.select { |i| !i.empty? }
+
+      if !appendix_items.empty?
+        appendix = ('[' + appendix_items.join('|') + ']').yellow
+        padding = ' ' * 14
+      end
+
+      resource_name = options[0].referenced_resource
 
       # TODO: translate the description
-      ["--#{flag}#{appendix} #{flag.upcase}", '              '+flag]
+      ["#{prefix}#{appendix} #{resource_name.upcase}", "#{padding}#{resource_name.gsub('_', ' ').capitalize}"]
+    end
+
+    def common_prefix(items)
+      shortest_length = items.map(&:length).min
+      i = 0
+      while (i < shortest_length) && chars_equal(i, items)
+        i += 1
+      end
+      items[0][0, i]
+    end
+
+    def chars_equal(index, items)
+      items.map {|i| i[index] }.uniq.length == 1
     end
 
     def is_squeezable?(item)
-      SQUEEZABLE.each do |flag|
-        return true if item[0] =~ /--#{flag}[ -]/
-      end
-      false
+      !item.referenced_resource.nil?
     end
 
     def is_option_list?(items)
       (items.size > 0) && (items[0].is_a?(Clamp::Option::Definition))
     end
   end
-
-
-
 end
 
 
 class HammerCLI::AbstractCommand
   def help
-    self.class.help(invocation_path, HammerCLIExperimental::CondensedBuilder.new)
+    self.class.help(invocation_path, HammerCLIExperimental::CondensedBuilder.new(context[:is_tty?]))
   end
 end
